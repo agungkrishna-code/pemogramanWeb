b787df05618c94fe1774b3f0510994877d59c706
Agung Krishnananda <87513692+agungkrishna-code@users.noreply.github.com>
2022-11-20 14:20:41 +0800

    Add files via upload

---
+++ UTS_210030533/script.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+const prank = () => {
+  alert('Awas Nanti Kena Tembak 🔫')
+}
